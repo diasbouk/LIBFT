cb39053ac7008201da2e66f9098be065fd104209
sboukiou <boukdsaid@gmail.com>
2024-10-22 17:53:24 +0100

    Added ft_is* functions , need some updates later, (Return value currently restricted to 1 in succ case

---
+++ ft_isalnum.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 16:12:08 by sboukiou          #+#    #+#             */
+/*   Updated: 2024/10/22 16:13:33 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
+}
+
+static int	ft_isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+		return (1);
+	return (0);
+}
+
+static int	ft_isalpha(int c)
+{
+	if (ft_islower(c) || ft_isupper(c))
+		return (1);
+	return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
+}
---
+++ ft_isalpha.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 15:04:26 by sboukiou          #+#    #+#             */
+/*   Updated: 2024/10/22 15:11:35 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+static int	ft_islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
+}
+
+static int	ft_isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+		return (1);
+	return (0);
+}
+
+int	ft_isalpha(int c)
+{
+	if (ft_islower(c) || ft_isupper(c))
+		return (1);
+	return (0);
+}
