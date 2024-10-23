79e9568cd019ebd1e54a0ed1e0ffc138447cc44e
sboukiou <boukdsaid@gmail.com>
2024-10-23 11:28:59 +0100

    Added new prototypes memmove, strlcat strlcpy ..

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:04:31 by sboukiou          #+#    #+#             */
-/*   Updated: 2024/10/22 18:23:41 by sboukiou         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:02:17 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,8 @@ void	ft_bzero(void *s, unsigned int n);
 void	*ft_memset(void *s, int c, unsigned int n);
 unsigned int	ft_strlen(const char *str);
 void	*ft_memcpy(void *dest, const void *src, unsigned int n);
+void	*ft_memmove(void *dest, const void *src, unsigned int n);
+char	*ft_strcpy(char *dest, const char *src);
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size);
+unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size);
 #endif
