c244fe263b59f697aec25548ad6b5a86d30ca9b8
sboukiou <boukdsaid@gmail.com>
2024-10-25 18:55:18 +0100

    Strlen test name replaced

--- tests/test_strlen.c
+++ tests/test_strlen.c
@@ -13,16 +13,25 @@ void    test_case(char *test)
     printf("String: %s\nExpected ---> %u\nGot ---> %u\n---------------------------\n", test, len_ex, len_got);
 }
 
+struct name {
+    int age;
+    char name[20];
+};
+
 int	main(void)
 {
     char *test_1 = "Test for sttr";
     int num = 1337;
     char test_2[90] = {'\0'};
     char test_3 = 'Z';
+    unsigned int pos = 133337;
+    struct name dias = {20, "dias"};
     test_case(test_1);
     test_case(test_2);
     test_case(&test_3);
     test_case((char *)(&num));
+    test_case((char *)(&pos));
+    test_case((char *)(&dias));
     test_case(NULL);
     return (0);
 }
