a8a8bbabb1ab2eb049f26e6dd6e0c1109b3f84e7
sboukiou <boukdsaid@gmail.com>
2024-10-25 03:03:07 +0100

    Added more prototypes + put functions in order

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:04:31 by sboukiou          #+#    #+#             */
-/*   Updated: 2024/10/23 11:02:17 by sboukiou         ###   ########.fr       */
+/*   Updated: 2024/10/25 01:27:40 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,27 @@
 # define LIBFT_H
 
 int	ft_isalpha(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-void	ft_bzero(void *s, unsigned int n);
-void	*ft_memset(void *s, int c, unsigned int n);
 unsigned int	ft_strlen(const char *str);
+void	*ft_memset(void *s, int c, unsigned int n);
+void	ft_bzero(void *s, unsigned int n);
+void	ft_bzero(void *s, unsigned int n);
 void	*ft_memcpy(void *dest, const void *src, unsigned int n);
 void	*ft_memmove(void *dest, const void *src, unsigned int n);
-char	*ft_strcpy(char *dest, const char *src);
 unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size);
 unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int ft_strncmp(const char *s1, const char s2, unsigned int n);
+void	*ft_memchr(const void *s, int c, unsigned int n);
+int	ft_memcmp(const void *s1, const void *s2, unsigned int n);
+char	*ft_strnstr(const char *big, const char *little, unsigned int len);
+int	ft_atoi(const char *nptr);
+void	*ft_calloc(unsigned int nmemb, unsigned int size);
+char	*ft_strdup(const char *str);
 #endif
