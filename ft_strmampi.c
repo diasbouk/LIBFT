bdbab3319f50218b73d747bb9d9b0b690e8debd1
diasbouk <boukdsaid@gmail.com>
2024-10-30 18:55:26 +0100

    Adding funcs iterators

---
+++ ft_strmampi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmampi.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 18:18:49 by sboukiou          #+#    #+#             */
+/*   Updated: 2024/10/30 18:28:55 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_string;
+	unsigned int	index;
+
+	new_string = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!new_string)
+		return (NULL);
+	index = 0;
+	while (s[index])
+	{
+		new_string[index] = f(index, s[index]);
+		index++;
+	}
+	return (new_string);
+
+}
