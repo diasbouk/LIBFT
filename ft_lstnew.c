7d928bb8d8514a486aa4b44f66f8f53962ceb1b2
diasbouk <boukdsaid@gmail.com>
2024-10-31 19:24:26 +0100

    Lists struct files added: create new node, add at end ,start. count size delelte node...

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 12:44:32 by sboukiou          #+#    #+#             */
+/*   Updated: 2024/10/31 12:46:42 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	return (node);
+}
