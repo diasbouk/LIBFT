45730b6f79e36eb2f4a163d42f2b5b000c11b01a
sboukiou <sboukiou@student.1337.ma>
2024-11-03 16:35:45 +0100

    Norminette fixed for ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,14 +21,14 @@
 	* according to the strings comparing
 */
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	count;
 
 	count = 0;
 	while ((s1[count] || s2[count]) && count < n)
 	{
-		if (s1[count] !=  s2[count])
+		if (s1[count] != s2[count])
 			return (s1[count] - s2[count]);
 		count++;
 	}
