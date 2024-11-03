7c65fce84e7d615df1328837e4a7d990474e65c7
sboukiou <sboukiou@student.1337.ma>
2024-11-03 16:34:13 +0100

    Simplified strncmp , returning 0 at the end if no difference was found

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -25,8 +25,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	count;
 
-	if (n == 0)
-		return (0);
 	count = 0;
 	while ((s1[count] || s2[count]) && count < n)
 	{
@@ -34,5 +32,5 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 			return (s1[count] - s2[count]);
 		count++;
 	}
-	return (s1[count - 1] - s2[count - 1]);
+	return (0);
 }
