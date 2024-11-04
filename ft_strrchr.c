92c7d7969949f2c5d48669db7ac58c34b3fc13cb
sboukiou <sboukiou@student.1337.ma>
2024-11-04 19:34:21 +0100

    strrchr.v c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -12,18 +12,27 @@
 
 #include "libft.h"
 
+/**
+	* ft_strrchr - returns a pointer to the last occurrence of the character
+	* c in the string s
+	* @s: String to check
+	* @c: Character to find
+	* @Return: Address of last occurrence of c or NULL
+*/
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*address;
-	int	count;
+	int		count;
 
 	count = 0;
-	address = (void *)0;
+	address = NULL;
 	while (s[count])
 	{
 		if (s[count] == c)
 			address = (char *)(s + count);
 		count++;
 	}
+	if (c == 0)
+		return ((char *)(s + count));
 	return (address);
 }
