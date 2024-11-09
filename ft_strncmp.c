deed67ec2e1db196036ce9466a4f064ae8d1d656
sboukiou <sboukiou@student.1337.ma>
2024-11-09 14:35:14 +0100

    Casting the whole thing into unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -29,8 +29,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (count < n && (s1[count] || s2[count]))
 	{
 		if (s1[count] != s2[count])
-			return (s1[count] - s2[count]);
+			return ((unsigned char)s1[count] - (unsigned char)s2[count]);
 		count++;
 	}
-	return (s1[count] - s2[count]);
+	return ((unsigned char)s1[count] - (unsigned char)s2[count]);
 }
