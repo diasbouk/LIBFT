8b93ae6db7870f678226820eb4a83f1fafee622b
sboukiou <sboukiou@student.1337.ma>
2024-11-10 23:26:03 +0100

    removed condtions

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -20,7 +20,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 
 	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
-	if (size == 0 || srclen == 0)
+	if (size == 0)
 		return (srclen);
 	count = 0;
 	while (destlen + count < size - 1)
