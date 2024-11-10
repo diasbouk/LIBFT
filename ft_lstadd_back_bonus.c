375d8eae2e5b9eee74b2645d466e8bcdcfff4554
sboukiou <sboukiou@student.1337.ma>
2024-11-10 22:10:32 +0100

    add back bonuss

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -15,12 +15,14 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*temp;
-
 	if (!lst || !new)
 		return ;
 	temp = *lst;
 	if (!temp)
+	{
 		*lst = new;
+		return ;
+	}
 	while (temp->next)
 	{
 		temp = temp->next;
