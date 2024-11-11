4efb4e62a109ebbf4ca2bd72add57da964ae55f9
sboukiou <sboukiou@student.1337.ma>
2024-11-11 17:24:32 +0100

    Fixing the norminette

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -26,9 +26,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		return (NULL);
 	}
 	new_list_head = new_node;
-	lst = lst->next;
-	while (lst)
+	while (lst->next)
 	{
+		lst = lst->next;
 		new_node = ft_lstnew(f(lst->content));
 		if (!new_node)
 		{
@@ -37,7 +37,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list_head, new_node);
-		lst = lst->next;
 	}
 	return (new_list_head);
 }
