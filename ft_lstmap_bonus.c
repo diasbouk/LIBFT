0c605181095f85fd71c7ea13299cd76cccaa83dc
sboukiou <sboukiou@student.1337.ma>
2024-11-11 18:26:30 +0100

    Clearing only when list is already created and not the new_node to make sure all of the nodes are freed

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,13 +16,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_node;
 	t_list	*new_list_head;
+	t_list	*temp;
 
 	new_list_head = NULL;
+	temp = lst;
 	if (!lst || !f || !del)
 		return (NULL);
-	while (lst->next)
+	while (temp)
 	{
-		lst = lst->next;
 		new_node = ft_lstnew(f(lst->content));
 		if (!new_node)
 		{
@@ -33,6 +34,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			new_list_head = new_node;
 
 		ft_lstadd_back(&new_list_head, new_node);
+		temp = temp->next;
 	}
 	return (new_list_head);
 }
