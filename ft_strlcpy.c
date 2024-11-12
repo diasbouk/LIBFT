427fd213f2b5ce74a42666f9639f4c414a7123a0
sboukiou <sboukiou@student.1337.ma>
2024-11-12 11:52:25 +0100

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,17 +15,11 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	unsigned int	srclen;
-	unsigned int	count;
 
 	srclen = ft_strlen(src);
 	if (size == 0)
 		return (srclen);
-	count = 0;
-	while (count < size - 1 && src[count])
-	{
-		dest[count] = src[count];
-		count++;
-	}
-	dest[count] = '\0';
+	ft_memcpy(dest, src, size - 1);
+	dest[size] = '\0';
 	return (srclen);
 }
